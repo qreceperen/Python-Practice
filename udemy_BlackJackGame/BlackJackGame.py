13df4abfaec528325d47249c507e7033da46962e
Recep EREN <qreceperen@gmail.com>
2021-03-17 07:11:30 -0400

    DECK CLASS COMPLETION
    1- Deck list is printed for user
    2- deck is shuffled
    3- Single card is taken by pop command from the deck list.

--- udemy_BlackJackGame/BlackJackGame.py
+++ udemy_BlackJackGame/BlackJackGame.py
@@ -14,7 +14,7 @@ class Card():
         self.rank = rank
 
     def __str__(self):
-        return self.rank + ' of ' + self.suits
+        return self.rank + ' of ' + self.suit
 
 class Deck():
     def __init__(self):
@@ -24,5 +24,25 @@ class Deck():
                 # Nested card list is created for every possible card variation.
                 # Card variations are appended in deck list such as ((Heart, two))
                 # (Ace, three) , (Diamond, four))
-                self.deck.append(Class(suit, rank))
+                self.deck.append(Card(suit, rank))
 
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has: "+deck_comp
+        # all possible cards are written as string for user to see.(string method)
+
+
+    def shuffle(self):
+        random.shuffle(self.deck) # Appended desk list is shuffled.
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+        # from appended deck list, one card is taken (pop will take the first card
+        # by default, since the deck is shuffled before deal function first card
+        # always will be different.
+
+test_deck = Deck()
+print(test_deck)
