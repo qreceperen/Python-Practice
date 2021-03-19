5b1c84aff9c5d1efac593228156f5401ede4bee2
Recep EREN <qreceperen@gmail.com>
2021-03-19 07:46:24 -0400

    ACE ADJUSTMENT AND CHIP CLASS IS ADDED (MONEY TRACK DURING CARD GAME) 1-According to game rule, ace card type value can be 11 or 1 according to players card value. if player card value > 21 ace should be 1 else ace should be 11. ace_adjustment fuction allow us to use ace 11 or 1. In the values dictionary ace value is 11 by default. By considering this information , subtracting 10 from 11 (if player card value is <21) allow us to use ace accordingly.
    2- Chip class is added. It tracks the money. If player win the bet def win_def function
    increase the total value (total represents the money at the begining, By default it is 100)
    If player lose the bet, def lose_bet function decrease the total according to player bet.

--- udemy_BlackJackGame/BlackJackGame.py
+++ udemy_BlackJackGame/BlackJackGame.py
@@ -2,10 +2,12 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
-          'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 playing = True
+
+
 # It starts playing. When False Game is over.
 
 class Card():
@@ -16,9 +18,10 @@ class Card():
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
+
 class Deck():
     def __init__(self):
-        self.deck = [] # All 52 cards possibilities will go in deck list.
+        self.deck = []  # All 52 cards possibilities will go in deck list.
         for suit in suits:
             for rank in ranks:
                 # Nested card list is created for every possible card variation.
@@ -30,12 +33,11 @@ class Deck():
         deck_comp = ''
         for card in self.deck:
             deck_comp += '\n' + card.__str__()
-        return "The deck has: "+deck_comp
+        return "The deck has: " + deck_comp
         # all possible cards are written as string for user to see.(string method)
 
-
     def shuffle(self):
-        random.shuffle(self.deck) # Appended desk list is shuffled.
+        random.shuffle(self.deck)  # Appended desk list is shuffled.
 
     def deal(self):
         single_card = self.deck.pop()
@@ -43,6 +45,8 @@ class Deck():
         # from appended deck list, one card is taken (pop will take the first card
         # by default, since the deck is shuffled before deal function first card
         # always will be different.
+
+
 class Hand():
     # Hand class represents the cards in player's hand.
     def __init__(self):
@@ -52,7 +56,7 @@ class Hand():
         self.aces = 0
         # Aces can be 1 or 11 (Their value is dynamic)
 
-    def add_card(self,card):
+    def add_card(self, card):
         # This function will add a card from deck to player's hand.
         self.cards.append(card)
         # Card attribute comes from deck class not from Card class.
@@ -63,12 +67,39 @@ class Hand():
         # values dictionary. From dictionary integer (rank) will be assigned to card
         # in order to make mathematical comparisons between players card values.
 
-
         # track aces
         if card.rank == 'Ace':
             self.aces += 1
         # this command identify is there aces in the player's hand or not
         # by increasing self.aces value
+
+    def adjust_for_ace(self):
+        # If total value > 21 and still have an ace than change my ace to be 1 instead of 11
+        while self.value > 21 and self.ace > 0:
+            self.value -= 10
+            self.aces -= 1
+            # Ace value in the values dictionary is 11 by default. According to
+            # game rule ace can be 11 or 1. If players card value is more than 21
+            # ace should be 1, else ace should be 11. (self.value -=10 , 11-10 =1 ace
+            # value becomes 1 if self.value >21)
+            # self.aces decreased by one after using ace value.
+
+
+class Chips:
+    # You can think chips as money gaining and losing process
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+        # Add to total money according to player's bet.
+
+    def lose_bet(self):
+        self.total -= self.bet
+        # Subtract from total according to player's bet.
+
+
 
 
 test_deck = Deck()
@@ -84,30 +115,3 @@ print(test_player.value)
 
 test_player.add_card(test_deck.deal())
 print(test_player.value)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
