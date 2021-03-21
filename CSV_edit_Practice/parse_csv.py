2c3b02f794d664f68330372c591dd1ac58ee0b8c
Recep EREN <qreceperen@gmail.com>
2021-03-21 14:18:41 -0400

    NEW CSV IS CREATED 1- new_names.csv is created as new_file 2- csv_writer is the variable for the new_file 3- Each line is iterated form csv_reader( names.csv file) and written into new_names.csv file. with \t delimiter.

--- CSV_edit_Practice/parse_csv.py
+++ CSV_edit_Practice/parse_csv.py
@@ -3,9 +3,15 @@
 import csv
 
 with open('names.csv' , 'r') as csv_file: # csv_file is what you want to call file
-
     csv_reader = csv.reader(csv_file)
-    # csv_reader is variable name it can be anything you want
 
-    for line in csv_reader:
-        print(line[2])
+    with open ('new_names.csv' ,'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter ='\t')
+    # a new csv file is created
+    # csv_writer is the variable it can be any name.
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
+
+        # All the rows are taken from names.csv file through csv_reader variable
+        # all rows are written in a new_names.csv file through csv_writer.writerow(line)
