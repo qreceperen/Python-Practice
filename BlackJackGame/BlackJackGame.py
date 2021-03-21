3652f4ae3c6b9e39bef77a7aaf350cb517d3a905
Recep EREN <qreceperen@gmail.com>
2021-03-21 13:26:29 -0400

    Folder names are updated (CORRECTED)

---
+++ BlackJackGame/BlackJackGame.py
@@ -0,0 +1,118 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+playing = True
+
+
+# It starts playing. When False Game is over.
+
+class Card():
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+
+class Deck():
+    def __init__(self):
+        self.deck = []  # All 52 cards possibilities will go in deck list.
+        for suit in suits:
+            for rank in ranks:
+                # Nested card list is created for every possible card variation.
+                # Card variations are appended in deck list such as ((Heart, two))
+                # (Ace, three) , (Diamond, four))
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has: " + deck_comp
+        # all possible cards are written as string for user to see.(string method)
+
+    def shuffle(self):
+        random.shuffle(self.deck)  # Appended desk list is shuffled.
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+        # from appended deck list, one card is taken (pop will take the first card
+        # by default, since the deck is shuffled before deal function first card
+        # always will be different.
+
+
+class Hand():
+    # Hand class represents the cards in player's hand.
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        # Tracks the value of the cards that player holding.
+        self.aces = 0
+        # Aces can be 1 or 11 (Their value is dynamic)
+
+    def add_card(self, card):
+        # This function will add a card from deck to player's hand.
+        self.cards.append(card)
+        # Card attribute comes from deck class not from Card class.
+        # from Deck.deal()--> single Card(suit,rank)
+        self.value += values[card.rank]
+        # self.value will increase according to card that delivered to player.
+        # card.rank (it becomes key in values dictionary) will be checked from
+        # values dictionary. From dictionary integer (rank) will be assigned to card
+        # in order to make mathematical comparisons between players card values.
+
+        # track aces
+        if card.rank == 'Ace':
+            self.aces += 1
+        # this command identify is there aces in the player's hand or not
+        # by increasing self.aces value
+
+    def adjust_for_ace(self):
+        # If total value > 21 and still have an ace than change my ace to be 1 instead of 11
+        while self.value > 21 and self.ace > 0:
+            self.value -= 10
+            self.aces -= 1
+            # Ace value in the values dictionary is 11 by default. According to
+            # game rule ace can be 11 or 1. If players card value is more than 21
+            # ace should be 1, else ace should be 11. (self.value -=10 , 11-10 =1 ace
+            # value becomes 1 if self.value >21)
+            # self.aces decreased by one after using ace value.
+
+
+class Chips:
+    # You can think chips as money gaining and losing process
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+        # Add to total money according to player's bet.
+
+    def lose_bet(self):
+        self.total -= self.bet
+        # Subtract from total according to player's bet.
+
+
+
+
+test_deck = Deck()
+test_deck.shuffle()
+
+
+# PLAYER
+test_player = Hand()
+# Deal 1 card from the deck CARD (suit, rank)
+pulled_card = test_deck.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.value)
+
+test_player.add_card(test_deck.deal())
+print(test_player.value)
---
+++ WarCardGame/WarCardGame.py
@@ -0,0 +1,180 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+# ranks tuple is added to be sure rank name in the ranks and rank name in the
+# values match to each other.
+
+
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack',
+         'Queen', 'King', 'Ace')
+
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9,
+          'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+
+
+class Card:
+    # Suit is type of card such as Heart, Diamond etc.
+    # Rank is the rank of the card(Written on the card)
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    # Print the suit and value
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                # Create the Card Object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        # shuffle the card list internally it shuffles the all_cars list
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        # grabbing one of the card in the list. Remove one card from the
+        # all_card list and use it whenever need it.
+        return self.all_cards.pop()
+
+
+class Player:
+    # This class hold a player's current list of cards.
+    def __init__(self, name):
+        # name is for player name
+        # all card refers the card that player holds.
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        # this function is going to remove a card from player decks.
+        # Card is going to be removed from the top of the deck
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        # add_card function is going to add card to players deck when player win cards.
+        # Cards will be added to the bottom of the player's deck.
+        if type(new_cards) == type([]):
+            # During the waw situation multiple cards might be gained. In this type of
+            # situation extend method will combine the card deck with player's deck.
+            self.all_cards.extend(new_cards)
+        else:
+            # if player gains only one card.
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+
+# GAME SETUP
+# players are created from player class. Game is played with two player.
+player_one = Player("One")
+player_two = Player('Two')
+
+# players cards are created and shuffled.
+new_deck = Deck()
+new_deck.shuffle()
+
+# There are 52 cards in the deck that's why range is 26 (half of 52) in order to divide
+# into two evenly. Both players have 26 cards.
+# 26 Cards are added to both players according to Deck class deal_one function.
+for x in range(26):
+    # add_cards, divided cards (26) added to player one and player two
+    # add_cards function needs an argument. new_deck.deal_one is the argument given.
+    # This argument is called from Deck classes deal one function. It removes a card from
+    # players' deck(from top). In other words, player puts a card on the table from his deck.
+
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+# START THE GAME
+game_on = True
+# game on is added like that because we will use while loop.
+
+# while game on
+round_num = 0  # we will count the rounds.
+while game_on:
+    round_num += 1
+    print(f'Round {round_num}')
+
+    # Check one of the player is out of cards or not. Because if any player out of cards
+    # while loop should stop and one of the player wins the game.
+    if len (player_one.all_cards) == 0:
+        print(f'Player One, out of cards! Player two wins')
+        game_on = False
+        break
+
+    if len (player_two.all_cards) == 0:
+        print(f'Player Two, out of cards! Player one wins')
+        game_on = False
+        break
+
+    # START A NEW ROUND
+    player_one_cards = []
+    # This is the card player one leave on the table to start playing.
+    player_one_cards.append(player_one.remove_one())
+    # A card will be removed from top of player's deck and appended in player_one_cards
+    # list in order to compare player_one_cards and player two cards.
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+    # Same steps above is repeated for the player two.
+
+    # WHILE AT WAR
+
+    at_war = True
+    # When both player puts their card from top of their deck war starts. It means
+    # we need to compare their card value to see which player will take the card on table
+
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+        # player_one last card value(-1) is compared to player_two last card value(-1)
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            # player_one takes all the cards left on the table. player_one cards are
+            # updated by adding his current card and and player_two's card on the table.
+            at_war = False
+            # Current war is finished (war is comparison of cars) start new one.
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            at_war = False
+        # player two win the war. (situation reversed)
+
+
+        else:
+            print("WAR!")
+            '''According to game rule if players card value is equal each to each other
+            , each player should put three cards on the table and start  at_war again
+            with both players fourth card. According to fourth card value, winner
+            will take all the cards on the table. If fourth card value is equal again
+            players put another three cards and repeat process until one of them win'''
+
+            if len(player_one_cards) < 5:
+                print("Player One unable to declare war")
+                print("PLAYER TWO WINS!")
+                #  Normally game goes on until one of the players cards finish totaly.
+                # to shorten the game when a player has less than five card, the player loses the game
+                game_on = False
+                break
+
+            elif len(player_two_cards) < 5:
+                print("Player Two unable to declare war")
+                print("PLAYER ONE WINS!")
+                #  Normally game goes on until one of the players cards finish totaly.
+                # to shorten the game when a player has less than five card, the player loses the game
+                game_on = False
+                break
+
+            else:
+                # We have to add more cards to the table
+                for num in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
